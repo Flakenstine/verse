27c849ed905226a1a85fe1c3f262204615a088e4
Parker Smith <parker@yeetdev.com>
2019-11-12 18:24:56 -0800

    Fixed 2.0

--- src/containers/app.container.js
+++ src/containers/app.container.js
@@ -3,7 +3,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter, Route, Switch } from 'react-router-dom'
 
-<<<<<<< Updated upstream
 import Header from '../components/header/header.component'
 import Chat from '../pages/chat/chat.component'
 import Clubs from '../pages/clubs/clubs.component'
@@ -12,17 +11,6 @@ import Friends from '../pages/friends/friends.component'
 import ServerBrowser from '../components/serverbrowser/serverbrowser.component'
 import SocialNavigation from '../components/socialnavigation/socialnavigation.component'
 import ServerNavigation from '../components/servernavigation/servernavigation.component'
-=======
-import Header from '../components/header.component';
-import ServerBrowser from '../components/server-browser.component';
-import SocialNavigation from '../components/social-navigation.component';
-import Chat from '../components/chat.component';
-import Games from '../components/games.component';
-import Friends from '../components/friends.component';
-import Profile from '../components/profile.component';
->>>>>>> Stashed changes
-
-import Footer from '../components/footer/footer.component'
 
 class AppContainer extends React.Component {
   /**
@@ -30,8 +18,6 @@ class AppContainer extends React.Component {
    *
    * @memberof AppContainer
    */
-
-<<<<<<< Updated upstream
   hideChatSidebar = (location) => (location === '/' ? 'app__sidebar-left' : 'app__sidebar-left--alt')
 
   hideFriendSidebar = (location) => (location === '/' ? '' : 'none')
@@ -84,7 +70,6 @@ class AppContainer extends React.Component {
             <SocialNavigation />
           </div>
         </div>
-        <Footer />
       </div>
     )
   }
@@ -92,75 +77,6 @@ class AppContainer extends React.Component {
 
 AppContainer.propTypes = {
   location: PropTypes.object.isRequired,
-=======
-    render () {
-        const routes = [
-            {
-                path: "/",
-                exact: true,
-                component: Chat
-            },
-            {
-                path: "/games",
-                exact: true,
-                component: Games
-            },
-            {
-                path: "/friends",
-                exact: true,
-                component: Friends
-            },
-            {
-                path: "/profile",
-                exact: true,
-                component: Profile
-            },
-
-        ]
-
-        return (
-            <div className="container-fluid clearfix">
-                <div class="row flex-nowrap">
-                    <div class="col app-sidebar-left">
-                        <ServerBrowser></ServerBrowser>
-                    </div>
-                    <div class="col app-main">
-                        <Header></Header>
-                        <Switch>
-                             {routes.map((route, index) => (
-                                <Route key={index} exact={route.exact}  path={route.path} component={route.component}></Route>
-                             ))}
-                             <Route component={Chat}></Route>
-                         </Switch>
-                    </div>
-                    <div class="col app-sidebar-right">
-                        <SocialNavigation></SocialNavigation>
-                    </div>
-                </div>
-            </div>
-            // <div className="container-fluid">
-            //     <div className="draggableTop"></div>
-            //     <div className="row">
-            //         <div className="col app-side-bar-left">
-            //             <ServerBrowser></ServerBrowser>
-            //             <ServerNavigation></ServerNavigation>
-            //         </div>
-            //         <div class="col">
-            //             <Header></Header>
-            //             <Switch>
-            //                 {routes.map((route, index) => (
-            //                    <Route key={index} exact={route.exact}  path={route.path} component={route.component}></Route>
-            //                 ))}
-            //             </Switch>
-            //         </div>
-            //         <div class="col app-side-bar-right">
-            //             <SocialNavigation></SocialNavigation>
-            //         </div>
-            //     </div>
-            // </div>
-        )
-    }
->>>>>>> Stashed changes
 }
 
 export default withRouter(AppContainer)
