a9e302cb30e0843d00f4e381794c2ebcc92c8045
Vidsify <vidsify@hotmail.com>
2019-12-12 19:43:31 +0000

    More work on Profile

--- src/pages/profile/profile.component.jsx
+++ src/pages/profile/profile.component.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 import './profile.style.scss';
 
 import profilePicture from '../../images/unknown-profile.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faCommentAltDots } from '@fortawesome/pro-solid-svg-icons';
+import { faServer } from '@fortawesome/pro-light-svg-icons'
+import { faSwords, faGamepad, faAward, faEdit } from '@fortawesome/pro-regular-svg-icons'
 
 class Profile extends React.Component {
   render() {
@@ -29,6 +33,53 @@ class Profile extends React.Component {
                   <p className="member_since">Member Since November 6, 2016</p>
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="profile_header_bottom">
+            <div className="stats_area">
+              <ul className="stats">
+                <li>
+                  <a className="stats-image friends"><FontAwesomeIcon icon={faUsers}></FontAwesomeIcon></a>
+                  <div className="stats-info">
+                    <p className="stats-data">1,521</p>
+                    <p className="stats-text">Friends Added</p>
+                  </div>
+                </li>
+                <li>
+                  <a className="stats-image servers"><FontAwesomeIcon icon={faServer}></FontAwesomeIcon></a>
+                  <div className="stats-info">
+                    <p className="stats-data">21</p>
+                    <p className="stats-text">Servers Joined</p>
+                  </div>
+                </li>
+                <li>
+                  <a className="stats-image clubs"><FontAwesomeIcon icon={faSwords}></FontAwesomeIcon></a>
+                  <div className="stats-info">
+                    <p className="stats-data">3</p>
+                    <p className="stats-text">Clubs Joined</p>
+                  </div>
+                </li>
+                <li>
+                  <a className="stats-image awards"><FontAwesomeIcon icon={faAward}></FontAwesomeIcon></a>
+                  <div className="stats-info">
+                    <p className="stats-data">15</p>
+                    <p className="stats-text">Awards Received</p>
+                  </div>
+                </li>
+                <li>
+                  <a className="stats-image matches"><FontAwesomeIcon icon={faGamepad}></FontAwesomeIcon></a>
+                  <div className="stats-info">
+                    <p className="stats-data">20</p>
+                    <p className="stats-text">Matches Played</p>
+                  </div>
+                </li>
+                <li className="status-area">
+                  <a className="status-mark"><FontAwesomeIcon icon={faCommentAltDots}></FontAwesomeIcon></a>
+                  <p className="status-current">Current Status</p>
+                  <span className="status-text">Looking for a group to play Fortnite</span>
+                  <a className="status-edit"><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
