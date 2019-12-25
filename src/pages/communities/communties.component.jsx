a6544e42099ed2d1b2159f5c790a0e15eab0181f
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-12-25 16:24:32 -0700

    Convert unused components to functional components to stop react from bitching at us.

---
+++ src/pages/communities/communties.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Communities () {
+    return (
+        <div>
+            <p>User based and official communities.</p>
+        </div>
+    )
+}
+
+export default Communities
