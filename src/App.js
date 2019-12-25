8c9858808853e057341d5df6bd02761db3021453
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-12-25 15:42:56 -0700

    Convert App.js to stateless function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-/* eslint-disable react/prefer-stateless-function */
+// /* eslint-disable react/prefer-stateless-function */
+// import React from 'react'
+// import { BrowserRouter } from 'react-router-dom'
+// import AppContainer from './containers/app.container'
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <BrowserRouter>
+//         <AppContainer />
+//       </BrowserRouter>
+//     )
+//   }
+// }
+
+// export default App
+
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppContainer from './containers/app.container'
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <AppContainer />
-      </BrowserRouter>
-    )
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContainer />
+    </BrowserRouter>
+  );
 }
 
 export default App
