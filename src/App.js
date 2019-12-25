a8f420a7c078c67b27022a24d50f3a9cf90ece07
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-12-25 16:09:22 -0700

    Minor changes to master branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-// /* eslint-disable react/prefer-stateless-function */
-// import React from 'react'
-// import { BrowserRouter } from 'react-router-dom'
-// import AppContainer from './containers/app.container'
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <BrowserRouter>
-//         <AppContainer />
-//       </BrowserRouter>
-//     )
-//   }
-// }
-
-// export default App
-
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppContainer from './containers/app.container'
