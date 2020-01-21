ae7140856cb09e1b36a369971a6131b436239114
Marc DiFilippo <marc@thepalacemc.com>
2020-01-20 20:27:11 -0500

    Add register form

--- src/components/loginform/loginform.component.jsx
+++ src/components/loginform/loginform.component.jsx
@@ -16,27 +16,57 @@ class LoginForm extends React.Component {
       app.exit();
   };
 
+  handleRegister = () => {
+  };
+
   render() {
 
     return (
-        <form style={Object.assign({margin:'auto'})} onSubmit={e => {e.preventDefault(); this.handleLogin()}}>
-            <h4>Login</h4>
-            <div>
-                <h5>Email</h5>
+        <div>
+            <form style={Object.assign({margin:'auto'})} onSubmit={e => {e.preventDefault(); this.handleLogin()}}>
+                <h4>Login</h4>
                 <div>
-                    <input name="" type="email" placeholder="" aria-label="Email" autoComplete="off" maxLength="999" spellCheck="false" />
+                    <h5>Email</h5>
+                    <div>
+                        <input name="" type="email" placeholder="" aria-label="Email" autoComplete="off" maxLength="999" spellCheck="false" />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h5>Password</h5>
                 <div>
-                    <input name="" type="password" placeholder="" aria-label="Password" autoComplete="off" maxLength="999" spellCheck="false" />
+                    <h5>Password</h5>
+                    <div>
+                        <input name="" type="password" placeholder="" aria-label="Password" autoComplete="off" maxLength="999" spellCheck="false" />
+                    </div>
                 </div>
-            </div>
-            <button type="submit">
-                <div>Login</div>
-            </button>
-        </form>
+                <button type="submit">
+                    <div>Login</div>
+                </button>
+            </form>
+
+            <form style={Object.assign({margin:'auto'})} onSubmit={e => {e.preventDefault(); this.handleRegister()}}>
+                <h4>Register</h4>
+                <div>
+                    <h5>Email</h5>
+                    <div>
+                        <input name="" type="email" placeholder="" aria-label="Email" autoComplete="off" maxLength="999" spellCheck="false" />
+                    </div>
+                </div>
+                <div>
+                    <h5>Password</h5>
+                    <div>
+                        <input name="" type="password" placeholder="" aria-label="Password" autoComplete="off" maxLength="999" spellCheck="false" />
+                    </div>
+                </div>
+                <div>
+                    <h5>Confirm Password</h5>
+                    <div>
+                        <input name="" type="password" placeholder="" aria-label="Confirm Password" autoComplete="off" maxLength="999" spellCheck="false" />
+                    </div>
+                </div>
+                <button type="submit">
+                    <div>Register</div>
+                </button>
+            </form>
+        </div>
     )
   }
 }
