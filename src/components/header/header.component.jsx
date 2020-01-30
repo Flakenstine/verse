b5401a4ac62fbc82c4b033c12dd4b61c8ccccfdd
Flakenstine <isaac@thepalacemc.com>
2020-01-30 12:06:12 -0700

    Converted header to functional component instead of class based

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,74 +1,59 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faSearch, faStore, faUserPlus, faChevronDown,
-} from '@fortawesome/pro-light-svg-icons'
-import { faBell } from '@fortawesome/pro-solid-svg-icons'
-import { NavLink } from 'react-router-dom'
-import './header.component.scss'
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faStore, faUserPlus, faChevronDown} from '@fortawesome/pro-light-svg-icons';
+import { faBell } from '@fortawesome/pro-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
 
-  componentDidMount() {
+import './header.component.scss';
 
-  }
-
-  componentDidUpdate() {
-
-  }
-
-  render() {
-    return (
-      <header className="header">
-        <nav>
-          <ul>
-            <li className="nav-item">
-              <NavLink className="nav-link" exact to="/">Chat</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/games">Discovery</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/friends">Friends</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/profile">Profile</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div className="header__search">
-          <FontAwesomeIcon icon={faSearch} />
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/">Chat</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/games">Discovery</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/friends">Friends</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/profile">Profile</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="header__search">
+        <FontAwesomeIcon icon={faSearch} />
+        {' '}
+        <span>Search</span>
+      </div>
+      <div className="header__subnav">
+        <button type="button">
+          <NavLink to="/"><FontAwesomeIcon icon={faStore} /></NavLink>
+        </button>
+        <button type="button">
+          <NavLink to="/"><FontAwesomeIcon icon={faUserPlus} /></NavLink>
+        </button>
+        <button type="button">
+          <NavLink to="/"><FontAwesomeIcon icon={faBell} /></NavLink>
+        </button>
+      </div>
+      <div className="header__currentUser">
+        <img alt="" className="avatar" />
+        <div className="username">
+          Welcome,
           {' '}
-          <span>Search</span>
+          <span>Brant</span>
+          {' '}
+          <span className="dropdown_selector"><FontAwesomeIcon icon={faChevronDown} /></span>
         </div>
-        <div className="header__subnav">
-          <button type="button">
-            <NavLink to="/"><FontAwesomeIcon icon={faStore} /></NavLink>
-          </button>
-          <button type="button">
-            <NavLink to="/"><FontAwesomeIcon icon={faUserPlus} /></NavLink>
-          </button>
-          <button type="button">
-            <NavLink to="/"><FontAwesomeIcon icon={faBell} /></NavLink>
-          </button>
-        </div>
-        <div className="header__currentUser">
-          <img alt="" className="avatar" />
-          <div className="username">
-            Welcome,
-            {' '}
-            <span>Brant</span>
-            {' '}
-            <span className="dropdown_selector"><FontAwesomeIcon icon={faChevronDown} /></span>
-          </div>
-        </div>
-      </header>
-    )
-  }
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
