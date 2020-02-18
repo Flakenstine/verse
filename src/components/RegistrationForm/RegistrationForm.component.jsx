3ab9f43c7376d83d6d4298ee90e012b3d268c6c0
Flakenstine <isaac@thepalacemc.com>
2020-02-17 19:41:49 -0700

    Wrote base logic, having issues with post request

--- src/components/RegistrationForm/RegistrationForm.component.jsx
+++ src/components/RegistrationForm/RegistrationForm.component.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 import EmailField from "./EmailField";
 import PasswordField from "./PasswordField";
-import Axios from "axios";
+import axios from "axios";
+import { faTrafficLightSlow } from "@fortawesome/pro-regular-svg-icons";
 
 
 class RegistrationForm extends Component {
 
-    state = { emailValid: false, emailValue: "", passwordValid: false, passwordValue: "", hasError: false, error: "" }
+    state = { emailValid: false, emailValue: "", passwordValid: false, passwordValue: "", hasError: false, error: "", hasSuccess: false  }
 
     emailStateChanged = (field) => {
         this.setState({ emailValid: field.errors.length === 0, emailValue: field.value});
-        console.log(this.state.emailValid);
     }
 
     passwordStateChanged = (field) => {
         this.setState({ passwordValid: field.errors.length === 0, passwordValue: field.value});
-        console.log(this.state.passwordValid);
     }
+t
 
      
     handleRegister = (e) => {
@@ -26,21 +26,25 @@ class RegistrationForm extends Component {
         const email = this.state.emailValue;
         const password = this.state.passwordValue;
 
-        if ( email.length !== 0 && password.length !== 0 ) {
-            Axios.post('https://api.verseapp.co/v1/users/create', {
+        console.log(email);
+        console.log(password);
+
+        // prevent the user from submitting if the form inputs are empty
+        if (email.length !== 0 && password.length !== 0) {
+            axios.post('https://api.verseapp.co/v1/users/create', {
                 email,
                 password
-            }).then((result)=> {
-                // todo: Actually implement proper login logic
-                if (result.status.success) {
-                    console.log(result);
-                    // instead of logging the response here I am going to send the user a message stating that registration was a success and they should check their email for further instructions.
-                } else {
-                    console.log("There was an error!");
-                }
-            }).catch((error) => {
-                console.log(error.result);
             })
+            .then((response) => {
+                if (response.status.success) {
+                    console.log(response);
+                } else {
+                    // error checking will go here
+                    console.log("An error occured!");
+                }
+            }, (error) => {
+                console.log(error);
+            });
         } else {
             this.setState({hasError: true, error: "A username and password are required to make an account!"});
         }
@@ -52,6 +56,7 @@ class RegistrationForm extends Component {
 
         return(
             <div className="form-container d-table-cell position-relative align-middle">
+                {/* { && "You have successfully created a verse account! In order to use verse, we will need to verify your account. Please check your registered email for further instructions!"} */}
                 <form onSubmit={this.handleRegister} noValidate>
                     <div className="d-flex flex-row justify-content-between align-items-center px-3 mb-5">
                         <legend className="form-label mb-0">Create an Account</legend>
@@ -60,7 +65,7 @@ class RegistrationForm extends Component {
                     <div className="py-5 border-gray border-top border-bottom">
                         <EmailField fieldId="email" label="Email" placeholder="" onStateChanged={this.emailStateChanged} required />
                         <PasswordField fieldId="password" label="Password" placeholder="" onStateChanged={this.passwordStateChanged} thresholdLength={7} minStrength={3} required />
-                        <button type="submit" disabled={!formValidated} className="btn btn-primary text-uppercase px-3 py2">Register</button>
+                        <button type="submit"  className="btn btn-primary text-uppercase px-3 py2">Register</button>
                     </div>
                 </form>
             </div>
