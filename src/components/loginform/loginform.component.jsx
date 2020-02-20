5e11e47d124d155eebc9d975d8a3827931c3158c
Vidsify <vidsify@hotmail.com>
2020-02-20 22:13:30 +0000

    Fixed styling after merge

--- src/components/loginform/loginform.component.jsx
+++ src/components/loginform/loginform.component.jsx
@@ -10,36 +10,36 @@ import verseLogo from '../../images/verse-logo-gold.png'
 const apiUtil = require('../../utils/apiUtil');
 
 class LoginForm extends React.Component {
-	
-	state = { email: "", password: "", hasError: false, error: "" }
 
-	fieldValueChanged = (field) => state => this.setState({ [field]: state.value });
+    state = { email: "", password: "", hasError: false, error: "" }
 
-	emailChanged = this.fieldValueChanged('email');
-	passwordChanged = this.fieldValueChanged('password');
+    fieldValueChanged = (field) => state => this.setState({ [field]: state.value });
 
-	handleLogin = (e) => {
-		e.preventDefault();
+    emailChanged = this.fieldValueChanged('email');
+    passwordChanged = this.fieldValueChanged('password');
 
-		const email = this.state.email;
-		const password = this.state.password;
+    handleLogin = (e) => {
+        e.preventDefault();
 
-		if (email.length !== 0 || password.length !== 0) {
-			apiUtil.handleLogin(email, password, (error, response) => {
-				if (error) {
-					this.setState({hasError: true, error: response.result});
-				} else {
-					if (response.data.success) {
-						this.props.logUserIn(response.data.authToken, response.data.userId);
-					} else {
-						this.setState({hasError: true, error: "Login credentials are invalid"})
-					}
-				}
-			});
-		} else {
-			this.setState({hasError: true, error: "Email and Password are required!"});
-		}
-	}
+        const email = this.state.email;
+        const password = this.state.password;
+
+        if (email.length !== 0 || password.length !== 0) {
+            apiUtil.handleLogin(email, password, (error, response) => {
+                if (error) {
+                    this.setState({ hasError: true, error: response.result });
+                } else {
+                    if (response.data.success) {
+                        this.props.logUserIn(response.data.authToken, response.data.userId);
+                    } else {
+                        this.setState({ hasError: true, error: "Login credentials are invalid" })
+                    }
+                }
+            });
+        } else {
+            this.setState({ hasError: true, error: "Email and Password are required!" });
+        }
+    }
 
     render() {
         return (
@@ -49,22 +49,25 @@ class LoginForm extends React.Component {
                     <div className="card">
                         <div className="card-header">
                             <img className="verseLogo" src={verseLogo}></img>
-                            <p>Sign in or create an account</p>
+                            <p className="signInText">Sign in or create an account</p>
                         </div>
                         <div className="card-body">
                             <form noValidate onSubmit={this.handleLogin}>
                                 <div className="emailArea">
                                     <label htmlFor="email">Email</label>
-                                    <EmailField fieldId="email" label="Email" placeholder="" onStateChanged={this.emailChanged} required />
+                                    <EmailField className="user-input" fieldId="email" placeholder="" onStateChanged={this.emailChanged} required />
                                 </div>
 
                                 <div className="passwordArea">
                                     <label htmlFor="password">Password</label>
-                                    <FormField type="password" fieldId="password" label="Password" placeholder="" onStateChanged={this.passwordChanged} required />
+                                    <FormField className="user-input" type="password" fieldId="password" placeholder="" onStateChanged={this.passwordChanged} required />
                                 </div>
                                 <p className="forgotp">Forgotten Password?</p>
 
-                                <input type="submit" value="Login" />
+                                <div className="loginButton">
+                                    <input type="submit" value="Login"/>
+                                </div>
+
                                 <p className="createAccount">Need an account?<a> Click here!</a></p>
                             </form>
                         </div>
