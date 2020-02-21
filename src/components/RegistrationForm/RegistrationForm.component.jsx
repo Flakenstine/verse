7130aeab0b092dde87860892a12c20f4b9c10cfc
Vidsify <vidsify@hotmail.com>
2020-02-21 12:33:39 +0000

    Basic register form

--- src/components/RegistrationForm/RegistrationForm.component.jsx
+++ src/components/RegistrationForm/RegistrationForm.component.jsx
@@ -3,19 +3,22 @@ import EmailField from "./EmailField";
 import PasswordField from "./PasswordField";
 import axios from "axios";
 
+import verseLogo from '../../images/verse-logo-gold.png'
+import './RegistrationForm.component.scss'
+
 class RegistrationForm extends Component {
 
-    state = { emailValid: false, emailValue: "", passwordValid: false, passwordValue: "", hasError: false, error: "", hasSuccess: false  }
+    state = { emailValid: false, emailValue: "", passwordValid: false, passwordValue: "", hasError: false, error: "", hasSuccess: false }
 
     emailStateChanged = (field) => {
-        this.setState({ emailValid: field.errors.length === 0, emailValue: field.value});
+        this.setState({ emailValid: field.errors.length === 0, emailValue: field.value });
     }
 
     passwordStateChanged = (field) => {
-        this.setState({ passwordValid: field.errors.length === 0, passwordValue: field.value});
+        this.setState({ passwordValid: field.errors.length === 0, passwordValue: field.value });
     }
 
-     
+
     handleRegister = (e) => {
         e.preventDefault();
         this.setState({ hasError: false });
@@ -27,50 +30,66 @@ class RegistrationForm extends Component {
             email,
             password
         })
-        .then((response) => {
-            if (response.status.success) {
-                this.setState({ hasSuccess: true });
-            }
-        }, (error) => {
-            const errorCode = error.response.data.errors[0].code;
+            .then((response) => {
+                if (response.status.success) {
+                    this.setState({ hasSuccess: true });
+                }
+            }, (error) => {
+                const errorCode = error.response.data.errors[0].code;
 
-            switch(errorCode) {
-                case 2004:
-                    this.setState({ hasError: true, error: error.response.data.errors[0].message});
-                    break;
-                case 2006:
-                    this.setState({ hasError: true, error: error.response.data.errors[0].message});
-                    break;
-                default:
-                    this.setState({hasError: true, error: "An unknown error occured while creating an account!"});
-            }
-        });
-    } 
+                switch (errorCode) {
+                    case 2004:
+                        this.setState({ hasError: true, error: error.response.data.errors[0].message });
+                        break;
+                    case 2006:
+                        this.setState({ hasError: true, error: error.response.data.errors[0].message });
+                        break;
+                    default:
+                        this.setState({ hasError: true, error: "An unknown error occured while creating an account!" });
+                }
+            });
+    }
 
     render() {
-        const { emailValid, passwordValid} = this.state;
+        const { emailValid, passwordValid } = this.state;
         const formValidated = emailValid && passwordValid;
 
-        return(
-            <div className="form-container d-table-cell position-relative align-middle">
-                { this.state.hasError && <div className="alert alert-danger">{ this.state.error }</div> }
-                { this.state.hasSuccess && "You have successfully created a verse account! In order to use verse, we will need to verify your account. Please check your registered email for further instructions!" }
-                <form onSubmit={this.handleRegister} noValidate>
-                    <div className="d-flex flex-row justify-content-between align-items-center px-3 mb-5">
-                        <legend className="form-label mb-0">Create an Account</legend>
-                    </div>
+        return (
+            <div className="registerArea">
+                <div className="bg-image"></div>
+                {this.state.hasError && <div className="alert alert-danger">{this.state.error}</div>}
+                {this.state.hasSuccess && "You have successfully created a verse account! In order to use verse, we will need to verify your account. Please check your registered email for further instructions!"}
+                <div className="register--wrapper">
+                    <div className="card">
+                        <div className="card-header">
+                            <img className="verseLogo" src={verseLogo}></img>
+                            <legend className="form-label mb-0">Create an Account</legend>
+                        </div>
+                        <div className="card-body">
+                            <form onSubmit={this.handleRegister} noValidate>
+                                <div className="emailArea">
+                                    <label htmlFor="email">Email</label>
+                                    <EmailField fieldId="email" placeholder="" onStateChanged={this.emailStateChanged} required />
+                                </div>
 
-                    <div className="py-5 border-gray border-top border-bottom">
-                        <EmailField fieldId="email" label="Email" placeholder="" onStateChanged={this.emailStateChanged} required />
-                        <PasswordField fieldId="password" label="Password" placeholder="" onStateChanged={this.passwordStateChanged} thresholdLength={7} minStrength={3} required />
-                        <button type="submit" disabled={!formValidated}  className="btn btn-primary text-uppercase px-3 py2">Register</button>
+                                <div className="passwordArea">
+                                    <label htmlFor="password">Password</label>
+                                    <PasswordField fieldId="password" placeholder="" onStateChanged={this.passwordStateChanged} thresholdLength={7} minStrength={3} required />
+                                </div>
+
+                                <div className="registerButton">
+                                    <button type="submit" disabled={!formValidated} className="btn">Register</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </form>
+                </div>
+
             </div>
         );
 
     }
-} 
+}
 
 
 export default RegistrationForm;
