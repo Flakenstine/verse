6bf34130960fba159ce5a31d0e25f45012959151
Marc <legobuilder0813@gmail.com>
2020-02-27 16:43:42 -0500

    Fix spacing cuz I'm _that_ guy

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -18,70 +18,68 @@ class App extends Component {
     }
 
     componentDidMount () {
-			this.checkUserCredentials(true);
-			this.logUserIn = this.logUserIn.bind(this);
-		}
-		
-		checkUserCredentials(init) {
-			if (getAuthStore().has("authToken") && getAuthStore().has("userId")) {
-				let authToken = getAuthStore().get("authToken");
-				let userId = getAuthStore().get("userId");
+		this.checkUserCredentials(true);
+		this.logUserIn = this.logUserIn.bind(this);
+	}
+	
+	checkUserCredentials(init) {
+		if (getAuthStore().has("authToken") && getAuthStore().has("userId")) {
+			let authToken = getAuthStore().get("authToken");
+			let userId = getAuthStore().get("userId");
 
-				verifyCredentials(authToken, userId, (error, response) => {
-					if (error) {
-						this.setState({
-							userAuthed: false,
-							loading: false
-						});
-					} else {
-						this.setState({
-							userAuthed: response.data.success,
-							loading: false
-						});
-					}
-				});
-
-				if (init) {
-					this.setState({
-						userAuthed: false,
-						loading: true
-					});
-				}
-			} else {
-				if (init) {
+			verifyCredentials(authToken, userId, (error, response) => {
+				if (error) {
 					this.setState({
 						userAuthed: false,
 						loading: false
 					});
 				} else {
 					this.setState({
-						userAuthed: false,
+						userAuthed: response.data.success,
 						loading: false
 					});
 				}
+			});
+
+			if (init) {
+				this.setState({
+					userAuthed: false,
+					loading: true
+				});
+			}
+		} else {
+			if (init) {
+				this.setState({
+					userAuthed: false,
+					loading: false
+				});
+			} else {
+				this.setState({
+					userAuthed: false,
+					loading: false
+				});
 			}
 		}
+	}
 
-		logUserIn (authToken, userId) {
-			getAuthStore().set("authToken", authToken);
-			getAuthStore().set("userId", userId);
-			if (window.navigator.platform === 'MacIntel') electron.remote.getCurrentWindow().setWindowButtonVisibility(false);
-			this.setState({
-				userAuthed: true
-			});
-		}
-
-
+	logUserIn (authToken, userId) {
+		getAuthStore().set("authToken", authToken);
+		getAuthStore().set("userId", userId);
+		if (window.navigator.platform === 'MacIntel') electron.remote.getCurrentWindow().setWindowButtonVisibility(false);
+		this.setState({
+			userAuthed: true
+		});
+	}
 
     render() {
-			return (
-				<div className="container-fluid">
-					<WindowsTitleBar />
-					<LoadSpinner visible={this.state.loading} />
-					<Main visible={this.state.userAuthed && !this.state.loading} />
-					<LoginComponent visible={!this.state.userAuthed && !this.state.loading} logUserIn={this.logUserIn} />
-				</div>
-			);
+		return (
+			<div className="container-fluid">
+				<WindowsTitleBar />
+				<LoadSpinner visible={this.state.loading} />
+				<Main visible={this.state.userAuthed && !this.state.loading} />
+				<LoginComponent visible={!this.state.userAuthed && !this.state.loading} logUserIn={this.logUserIn} />
+			</div>
+		);
     }
 }
 
